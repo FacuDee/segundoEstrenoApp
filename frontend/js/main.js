22a4b8e966e8a8db408261074442668c30d0977f
caroo1214 <carolinastoessel715@gmail.com>
2025-05-28 22:22:31 -0300

    subo footer y seccion preguntas

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -87,3 +87,20 @@ function initModals() {
     document.getElementById("overlay")?.classList.remove("hidden");
   });
 }
+
+
+/*preguntas comprar y vender*/
+document.addEventListener("DOMContentLoaded", function () {
+    var questions = document.querySelectorAll(".faq-title");
+
+    questions.forEach(function (question) {
+        question.addEventListener("click", function () {
+            var answer = this.nextElementSibling;
+            if (answer.style.display === "none" || answer.style.display === "") {
+                answer.style.display = "block";
+            } else {
+                answer.style.display = "none";
+            }
+        });
+    });
+});
