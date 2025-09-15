b59de8e3e5e24c551215b64d4f82b64d37c72fe8
FlorHeredia <florheredia95@hotmail.es>
2025-09-14 22:19:11 -0300

    implemente metodos get de prendas

--- backend/src/prenda/prenda.service.ts
+++ backend/src/prenda/prenda.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Prenda } from './prenda.entity';
 
 @Injectable()
-export class PrendaService {}
+export class PrendaService {
+    constructor(
+        @InjectRepository(Prenda)
+        private readonly prendaRepository: Repository<Prenda>,
+    ) {}
+
+    async findAll(): Promise<Prenda[]> {
+        return this.prendaRepository.find();
+    }
+    async findOne(id: number): Promise<Prenda | null> {
+        return this.prendaRepository.findOneBy({ id });
+    }
+}
