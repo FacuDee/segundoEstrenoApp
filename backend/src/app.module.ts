380fa48831ebdf770f734334dac2e9228d4f18aa
FacuDee <facudiorio@hotmail.com>
2025-09-26 16:17:17 -0300

    Crea componentes LoginModal y RegisterModal para iniciar sesion y registar usuario - dentro del Header

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -18,7 +18,7 @@ import { ConfigModule } from '@nestjs/config';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: '**** cambiar por la propia ****',
+      password: 'admin08_sql',
       database: 'segundo_estreno',
       autoLoadEntities: true,
       synchronize: false,
---
+++ frontend-2/src/components/header/Logo.jsx
@@ -0,0 +1,9 @@
+import { FaRecycle } from "react-icons/fa";
+
+const Logo = () => (
+  <a href="/" className="logo">
+    <FaRecycle className="logo-icon" />
+    <span>SEGUNDO ESTRENO</span>
+  </a>
+);
+export default Logo;
