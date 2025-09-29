3cfa3fc120b5a4b7cc05fc387b767b519f89aa01
Amelia1190 <lurbetamelialibertad@gmail.com>
2025-09-29 19:54:07 -0300

    Data

---
+++ frontend-2/src/data/PostData.js
@@ -0,0 +1,62 @@
+// src/data/PostData.js
+
+export const blogPosts = [
+  {
+    id: 1,
+    type: 'taller',
+    title: 'Taller de Costura Circular',
+    description: 'Aprendé a transformar prendas usadas en piezas únicas. Taller gratuito con inscripción previa.',
+    image: '/Blog/taller.webp',
+    imageAlt: 'Taller de costura',
+    link: 'https://docs.google.com/forms/d/e/1FAIpQLScP6sYGI0Dvpm8cHZyiJ7lK_2Ch_V6_CmB0hDAUOhBLGzN0Hw/viewform?usp=dialog',
+    linkText: 'Inscribite al Taller',
+    tag: 'Taller'
+  },
+  {
+    id: 2,
+    type: 'video',
+    title: 'Economía Circular en la Moda',
+    description: 'Dale una segunda vida a tu ropa. Descubrí cómo la economía circular transforma la industria textil. Es darle una nueva vida a lo que ya tenés.',
+    videoSrc: 'https://www.youtube.com/embed/mvGgdmUkIsQ?start=21',
+    videoTitle: 'Economía Circular en la Moda',
+    tag: 'Video'
+  },
+  {
+    id: 3,
+    type: 'video',
+    title: 'La moda circular en acción',
+    description: 'Este video muestra proyectos reales de moda sustentable y cómo impactan positivamente en el planeta.',
+    videoSrc: 'https://www.youtube.com/embed/RpkcmtvaEtQ',
+    videoTitle: '¿Cómo reciclar los 8 millones de toneladas de residuos textiles que se producen cada año en la UE?',
+    tag: 'Video'
+  },
+  {
+    id: 4,
+    type: 'video',
+    title: 'La moda circular explicada',
+    description: 'Un video inspirador que muestra cómo reutilizar ropa puede ser tendencia y salvar el planeta.',
+    videoSrc: 'https://www.youtube.com/embed/Vlv4FWZA18w',
+    videoTitle: 'Larga vida a la ropa: reciclaje textil',
+    tag: 'Video'
+  },
+  {
+    id: 5,
+    type: 'feria',
+    title: 'Feria de Intercambio',
+    description: 'Acá te dejamos info de las ferias semanales y mensuales. Traé tus prendas para vender o intercambiar. ¡Moda sin residuos!',
+    image: './frontend/images/Blog/feria.png',
+    imageAlt: 'Feria de moda circular',
+    tag: 'Feria'
+  },
+  {
+    id: 6,
+    type: 'noticia',
+    title: 'Moda Circular en los Medios',
+    description: 'La moda circular se posiciona como una tendencia clave en el consumo consciente. Esta nota de Ámbito Financiero explica cómo el reciclaje de prendas y el intercambio responsable están transformando la industria textil.',
+    image: 'https://d1whqwkn09gz4t.cloudfront.net/images/prensa/ambito-2024-03.webp',
+    imageAlt: 'Moda circular: la tendencia que cambió el paradigma en el consumo de indumentaria.',
+    link: 'https://www.ambito.com/negocios/la-moda-circular-cambio-el-paradigma-el-consumo-indumentaria-n5969584',
+    linkText: 'Leer artículo completo',
+    tag: 'Noticia'
+  }
+];
